79db410c9d24a8ab6b09e2ade2df47d23cee1644
xumingfei <18832892030@163.com>
2021-02-28 20:25:15 +0800

    test

--- spring-test/src/main/java/com/spring/test/demo/EnumTest.java
+++ spring-test/src/main/java/com/spring/test/demo/EnumTest.java
@@ -1,14 +1,18 @@
 package com.spring.test.demo;
 
 import com.spring.test.demo.enmu.IVMS_MGR_STATUS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author: Xiaofei
  * @DATE: 2021/2/27 13:03
  */
 public class EnumTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnumTest.class);
     public static void main(String[] args) {
-        System.out.println(IVMS_MGR_STATUS.NEW.getDescription());
-        System.out.println(IVMS_MGR_STATUS.optionValueOfKey("00").get());
+        LOGGER.warn(IVMS_MGR_STATUS.NEW.getDescription());
+        LOGGER.info("[{}]",IVMS_MGR_STATUS.optionValueOfKey("00").get());
+        LOGGER.error("[{}]",IVMS_MGR_STATUS.optionValueOfKey("00").get());
     }
 }
